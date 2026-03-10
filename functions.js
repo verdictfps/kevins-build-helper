fcb059c3f7473f9266a4f9aef22620d7031c8b7a
verdictfps <verdictfps@gmail.com>
2026-03-10 19:56:01 -0400

    cleanup, scrolls, etc. adding console information for bugs

--- functions.js
+++ functions.js
@@ -2,6 +2,7 @@
 // also ty stackoverflow
 
 window.mobileCheck = function() {
+    console.info("KBH: Checking for mobile device");
   let check = false;
   (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
   return check;
@@ -376,6 +377,7 @@ let oilsSpread = null;
 const coreSelections = new Map();
 
 function resetCoreSelections() {
+    console.info("KBH: Resetting core selections");
     coreSelections.set("weapon", {Name: "P38 Dirk", Value: "p38-dirk"});
     coreSelections.set("ench1", {Name: "None", Value: "static-no-selection"});
     coreSelections.set("ench2", {Name: "None", Value: "static-no-selection"});
@@ -397,6 +399,7 @@ const chamberNameIndexer = new Map();
 const barrelValueIndexer = new Map();
 
 function setBarrelValueIndexer() {
+    console.info("KBH: Setting barrel value indexer");
     barrelValueIndexer.set("none", "None");
     barrelValueIndexer.set("a12c-muzzle-brake", "A12C Muzzle Brake");
     barrelValueIndexer.set("aftermarket-haukland-silencer", "Aftermarket Haukland Silencer");
@@ -417,7 +420,7 @@ function setBarrelValueIndexer() {
 
 
 function setChamberValueIndexer() {
-
+console.info("KBH: Setting chamber value indexer");
     chamberValueIndexer.set("none", `${setDefaultChamber(coreSelections.get("weapon").Name)}`);
     chamberValueIndexer.set("chamber-chisel---.50-bmg", "Chamber Chisel - .50 BMG");
     chamberValueIndexer.set("chamber-chisel---12ga", "Chamber Chisel - 12Ga");
@@ -428,7 +431,7 @@ function setChamberValueIndexer() {
 }
 
 function setChamberNameIndexer() {
-
+console.info("KBH: Setting chamber name indexer");
     chamberValueIndexer.set("None", "none");
     chamberValueIndexer.set("Chamber Chisel - .50 BMG", "chamber-chisel---.50-bmg");
     chamberValueIndexer.set("Chamber Chisel - 12Ga", "chamber-chisel---12ga");
@@ -441,6 +444,7 @@ function setChamberNameIndexer() {
 let buildToEncode = null;
 
 function encodeBuildAsUri() {
+    console.info("KBH: Encoding build as Base64 string and applying to URL");
     buildToEncode = "build";
     function toBuild(value, key, map) {
         buildToEncode += value.Value;
@@ -456,6 +460,7 @@ function encodeBuildAsUri() {
 }
 
 function setBuildAsMetadata() {
+    console.info("KBH: Setting encoded string as site metadata");
     const encodedBuild = "#" + btoa(buildToEncode);
 
     let _desc = null;
@@ -478,15 +483,22 @@ function setBuildAsMetadata() {
 }
 
 function decodeUriAsBuild() {
+    console.info("KBH: Decoding URL to detect build");
     const currentURL = window.location.href;
     let split = currentURL.split("#");
     
-    let resplit = split[1]
-    let decoded = atob(resplit);
-    let split2 = decoded.split("build");
-    let finalSplit = split2[1].split("+");
-    let iterationSplit = 0
-    console.log(finalSplit);
+    if (typeof split[1] === 'undefined') {
+        console.info("KBH: No build found");
+    }
+    else {
+        console.info("KBH: Build found. Converting...");
+        let resplit = split[1]
+        let decoded = atob(resplit);
+        let split2 = decoded.split("build");
+        let finalSplit = split2[1].split("+");
+        let iterationSplit = 0
+        console.log(finalSplit);
+    }
 
     let defShantPass = false;
     setTimeout(() => {
@@ -579,7 +591,7 @@ function decodeUriAsBuild() {
 const weaponValueIndexer = new Map();
 
 function setWeaponValueIndexer() {
-
+console.info("KBH: Setting weapon value indexer");
     weaponValueIndexer.set("none", "None");
     weaponValueIndexer.set("beck-8", "Beck 8");
     weaponValueIndexer.set("bronco-89", "Bronco 89");
@@ -645,7 +657,7 @@ setWeaponValueIndexer()
 const weaponNameIndexer = new Map();
 
 function setWeaponNameIndexer() {
-
+console.info("KBH: Setting weapon name indexer");
     function addToWNI(value, key, map) {
         weaponNameIndexer.set(value, key);
     }
@@ -659,9 +671,13 @@ setWeaponNameIndexer()
 let scrollValueIndexer = new Map();
 
 function setScrollValueIndexer() {
-
+console.info("KBH: Setting scroll value indexer");
 
     scrollValueIndexer.set("none", "None");
+    scrollValueIndexer.set("scroll-of-dark", "Scroll of Dark");
+    scrollValueIndexer.set("scroll-of-earth", "Scroll of Earth");
+    scrollValueIndexer.set("scroll-of-embers", "Scroll of Embers");
+    scrollValueIndexer.set("scroll-of-frostbite", "Scroll of Frostbite");
     scrollValueIndexer.set("scroll-of-toxic-lobotomy", "Scroll of Toxic Lobotomy");
 
 }
@@ -670,6 +686,7 @@ setScrollValueIndexer()
 
 // It's time to shuffle
 function shuffle(array) {
+    console.info("KBH: Shuffling", array);
     let currentIndex = array.length;
 
     while (currentIndex != 0) {
@@ -704,6 +721,7 @@ function infoboxClear() {
 }
 
 function randomizeAllOils() {
+    console.info("KBH: Randomizing all enchantment slots");
     shallNotPass = true;
     document.getElementById("oils1selector").proDropdown.setValue("static-random-all-oils");
     document.getElementById("oils2selector").proDropdown.setValue("static-random-all-oils");
@@ -719,7 +737,7 @@ function randomizeAllOils() {
 }
 
 function resetAllOils() {
-
+console.info("KBH: Resetting all enchantment slots");
     shallNotPass = true;
     document.getElementById("oils1selector").proDropdown.setValue("static-no-selection");
     document.getElementById("oils2selector").proDropdown.setValue("static-no-selection");
@@ -736,6 +754,7 @@ function resetAllOils() {
 }
 
 function randomizeAllAttachments() {
+    console.info("KBH: Randomizing all attachment slots");
     shallNotPass = true;
     document.getElementById("barrelselector").proDropdown.setValue("static-random-barrel");
     document.getElementById("opticselector").proDropdown.setValue("static-random-optic");
@@ -749,7 +768,7 @@ function randomizeAllAttachments() {
 }
 
 function resetAllAttachments() {
-
+console.info("KBH: Resetting all attachment slots");
     shallNotPass = true;
     document.getElementById("barrelselector").proDropdown.setValue("none");
     document.getElementById("opticselector").proDropdown.setValue("none");
@@ -766,6 +785,7 @@ function resetAllAttachments() {
 }
 
 function commitSelection(buttonID, dropdownID, item) {
+    console.info("KBH: Committing", item, "to", dropdownID);
     shallNotPass = true;
     let getValue = coreSelections.get(item);
     let value = getValue.Value; 
@@ -774,6 +794,7 @@ function commitSelection(buttonID, dropdownID, item) {
 }
 
 function commitAllEnch() {
+    console.info("KBH: Committing all enchantments to their respective slots");
     commitSelection('buttonCommitOil1', 'oils1selector', 'ench1');
     commitSelection('buttonCommitOil2', 'oils2selector', 'ench2');
     commitSelection('buttonCommitOil3', 'oils3selector', 'ench3');
@@ -782,6 +803,7 @@ function commitAllEnch() {
 }
 
 function commitAllAtt() {
+    console.info("KBH: Committing all attachments to their respective slots");
     commitSelection('buttonCommitBarrel', 'barrelselector', 'barrel');
     commitSelection('buttonCommitOptic', 'opticselector', 'optic');
     commitSelection('buttonCommitLaser', 'laserselector', 'laser');
@@ -791,7 +813,7 @@ function commitAllAtt() {
 
 // Used to remove and replace oils to prevent dupes
 function oilRemover() {
-
+console.info("KBH: Beginning oil filtering process");
     var selector1Options = items.filter(i => i.li.dataset.dropdownId === "oils1selectorcollection");
     var selector2Options = items.filter(i => i.li.dataset.dropdownId === "oils2selectorcollection");
     var selector3Options = items.filter(i => i.li.dataset.dropdownId === "oils3selectorcollection");
@@ -805,6 +827,7 @@ function oilRemover() {
     let altSelector5Options = document.getElementById("oils5selector");
 
     function makeAllOilsVisible() {
+        console.info("KBH: Making all oils visible");
         if (window.mobileCheck === false) {
             for (var i = 0; i < selector1Options.length; i++) {
                 if (selector1Options[i].li.hidden === true) {
@@ -862,8 +885,8 @@ function oilRemover() {
     }
 
     function hideSelectedOils(value, key, map) {
+        console.info("KBH: Hiding ", key, value);
         if (window.mobileCheck === false) {
-        console.log(value, key)
             if (key.startsWith("ench")) {
                 let compOilRep = value.Name.Name.replaceAll(" ", "-");
                 let compOilLower = compOilRep.toLowerCase();
@@ -935,7 +958,7 @@ function oilRemover() {
 } 
 
 function attachmentFilter(evt) {
-
+console.info("KBH: Filtering attachments based on weapon selection");
     let selectorBarrel = document.getElementById("barrelselector");
     let selectorFiremode = document.getElementById("firemodeselector");
     let selectorChamber = document.getElementById("chamberselector");
@@ -1152,13 +1175,15 @@ let selType = null;
 let shallNotPass = false;
 
 async function rollOnPageLoad(flag, selector, selID, value, type) {
-
+    console.info("KBH: Beginning initial load");
     const result = await loadOrigWeapons();
     const result2 = await loadWeapons();
-    let selPageLoad = document.getElementById("weapons");
+    
+    
 
     setTimeout(() => {
-        
+        addAllEventListeners()
+        let selPageLoad = document.getElementById("weapons");
         selPageLoad.proDropdown.setValue("value"); 
         
         oilDefault = oilsData?.Oil["Default"];
@@ -1381,32 +1406,32 @@ function addName() {
 }
 
 async function loadChamber() {
-    const response = await fetch("./Chamber.json");
+    const response = await fetch("./itemdata/Chamber.json");
     chamberData = await response.json();
 }
 
 async function loadWeapons() {
-    const response = await fetch("./Weapons.json");
+    const response = await fetch("./itemdata/Weapons.json");
     weaponsData = await response.json();
 }
 
 async function loadOrigWeapons() {
-    const response = await fetch("./OrigWeapons.json");
+    const response = await fetch("./itemdata/OrigWeapons.json");
     weaponsOrigData = await response.json();
 }
 
 async function loadAttachments() {
-    const response = await fetch("./Attachments.json");
+    const response = await fetch("./itemdata/Attachments.json");
     attachmentsData = await response.json();
 }
 
 async function loadOils() {
-    const response = await fetch("./Oils.json");
+    const response = await fetch("./itemdata//Oils.json");
     oilsData = await response.json();
 }
 
 async function loadScrolls() {
-    const response = await fetch("./Scrolls.json");
+    const response = await fetch("./itemdata/Scrolls.json");
     scrollsData = await response.json();
 }
 
@@ -4248,7 +4273,7 @@ function mobileDropdownCheck(evt) {
             customDropHandler[i].hidden = true;
         }
     }
-    addAllEventListeners()
+   
     
 }
 
